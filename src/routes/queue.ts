f38fcd4213ce01b88390b1d6a92325363661ca29
Vinit Gupta <thevinitgupta@gmail.com>
2025-06-22 11:48:19 +0530

    add already completed validation and response structure

--- src/routes/queue.ts
+++ src/routes/queue.ts
@@ -12,17 +12,20 @@ const queueRoutes: FastifyPluginAsync = async (fastify) => {
         
         if (!blogId) {
             return reply.status(400).send({
+                message: "Blog id is required",
                 error: "Job data must contain blog id."
             });
         }
         addPostToQueue(blogId)
             .then(() => {
                 reply.send({
-                    message: "Job added to queue successfully."
+                    message: "Job added to queue successfully.",
+                    error : null
                 });
             })
             .catch((error) => {
                 reply.status(500).send({
+                    message: "Failed to add job to queue.",
                     error: error.message
                 });
             });
